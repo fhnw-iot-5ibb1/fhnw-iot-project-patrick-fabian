10c58d4ae6f11db55e08445a4d0d6e4a21e8b70b
Fabian Wildhaber <fabian.wildhaber@students.fhnw.ch>
2019-12-13 23:51:45 +0000

    cleanup thingspeakservice

--- raspi/thingSpeakService.py
+++ raspi/thingSpeakService.py
@@ -1,41 +1,40 @@
 import requests
 
-class ThingSpeakService():
 
-        def __init__(self):
-            self.url = 'https://api.thingspeak.com/channels/'
-            self.channel = 935198
-            self.apikey = '81SYGRV7PHQU25C8'
-            self.co2field = 'field1'
-            self.tempfield = 'field2'
-            self.humfield = 'field3'
+class ThingSpeakService:
 
+    def __init__(self):
+        self.url = 'https://api.thingspeak.com/channels/'
+        self.channel = 935198
+        self.api_key = '81SYGRV7PHQU25C8'
+        self.co2_field = 'field1'
+        self.temp_field = 'field2'
+        self.hum_field = 'field3'
 
+    def get_readings_json(self, nr_readings):
+        req = self.url + str(self.channel) + '/feeds.json?api_key=' + self.api_key + '&results=' + str(nr_readings)
+        response = requests.get(req)
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 404:
+            raise Exception('bad response ' + str(response.status_code))
 
-        def get_readings_json(self, nrReadings):
-            req = self.url + str(self.channel) + '/feeds.json?api_key=' + self.apikey + '&results=' + str(nrReadings)
-            response = requests.get(req)
-            if response.status_code == 200:
-                return response.json()
-            elif response.status_code == 404:
-                return 'error'
+    def get_field(self, field_name, num_readings):
+        json = self.get_readings_json(num_readings)
+        feeds = json.get('feeds')
+        if num_readings == 1:
+            return feeds[0].get(field_name)
+        else:
+            readings = []
+            for i in range(num_readings):
+                readings.append(feeds[i].get(field_name))
+            return readings
 
-        def get_field(self, fieldName, numReadings):
-            json = self.get_readings_json(numReadings)
-            feeds = json.get('feeds')
-            if numReadings == 1:
-                return feeds[0].get(fieldName)
-            else:
-                readings = []
-                for i in range(numReadings):
-                    readings.append(feeds[i].get(fieldName))
-                return readings
+    def get_co2(self, nr=1):
+        return self.get_field(self.co2_field, nr)
 
-        def get_co2(self, nr = 1):
-            return self.get_field(self.co2field, nr)
+    def get_temp(self, nr=1):
+        return self.get_field(self.temp_field, nr)
 
-        def get_temp(self, nr=1):
-            return self.get_field(self.tempfield, nr)
-
-        def get_hum(self, nr=1):
-            return self.get_field(self.humfield, nr)
+    def get_hum(self, nr=1):
+        return self.get_field(self.hum_field, nr)
