c27919eb083f64458b3c0619b926e8024bcad59e
Patrick Ackermann <patrick.ackermann@students.fhnw.ch>
2020-01-09 15:05:02 +0100

    Add real buzzer to airMonitor

--- raspi/airMonitor.py
+++ raspi/airMonitor.py
@@ -1,24 +1,32 @@
 # -*- coding: utf-8 -*-
 
-import time
+from time import time
 import button
 import thingSpeakService
+import buzzer
 from grove import grove_4_digit_display
 
 class StateMachine():
-    def __init__(self, button, display, sensor_service, buzzer):
+    def __init__(self, button, display, sensor_service, buzzer, rotation_sensor):
         self.button = button
         self.display = display
         self.sensor_service = sensor_service
         self.buzzer = buzzer
+        self.rotation_sensor = rotation_sensor
 
         self.state = self.co2
+        self.old_state = self.state
+        self.state_change_time = 0
         self.co2_threshold = 2000
         self.alarm_muted = False
         
         self.i = 0
 
     def run(self):
+        if self.state != self.old_state:
+          self.old_state = self.state
+          self.state_change_time = time()
+          
         self.state()
 
     def test_for_alarm(self):
@@ -41,8 +49,9 @@ class StateMachine():
         self.i = (self.i + 1) % (len(text) + 4)
         self.display.show(f"    {text}    "[self.i:self.i+4])
         
-    def show_co2(self):
-        co2 = self.sensor_service.get_co2()
+    def show_co2(self, co2=None):
+        if co2 is None:
+          co2 = self.sensor_service.get_co2()
         # print(f"co2: {co2}")
         co2str = f"{co2:4}"
         self.display.show(co2str[-4:])
@@ -73,7 +82,9 @@ class StateMachine():
         self.test_for_alarm()
 
     def set_co2_threshold(self):
-        print("set co2 threshold")
+        r = self.rotation_sensor.get()
+        
+        self.show_co2(self.co2_threshold)
 
         # next state
         self.state = self.co2
@@ -110,6 +121,9 @@ class Dummy():
     def reset(self):
         pass
         
+    def get(self):
+        return 1500
+        
     def turn_on(self):
         print("UUUUIIIIIIIUUUUUUIIII")
     
@@ -122,10 +136,10 @@ class Dummy():
 disp = grove_4_digit_display.Grove(16, 17, brightness=grove_4_digit_display.BRIGHT_HIGHEST)
 button = button.Button(pin=5, double_press_threshold=0.4)
 service = thingSpeakService.ThingSpeakService()
-# buzzer = buzzer.Buzzer()
+buzzer = buzzer.Buzzer()
 
 dummy = Dummy()
-SM = StateMachine(button=button, display=disp, sensor_service=service, buzzer=dummy)
+SM = StateMachine(button=button, display=disp, sensor_service=service, buzzer=buzzer, rotation_sensor=dummy)
 try:
   while True:
       SM.run()
