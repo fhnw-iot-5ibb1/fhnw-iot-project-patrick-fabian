35d802013fcbce3f0b1b5012fa475b5860d63525
Fabian Wildhaber <fabianwildhaber@gmail.com>
2020-01-09 16:47:08 +0100

    lower max servo rotation

--- raspi/servo.py
+++ raspi/servo.py
@@ -9,4 +9,4 @@ class Servo:
         self.gpio.set_servo_pulsewidth(self.servo_pin, 1500)
 
     def set_pulsewidth(self, pulsewidth):
-        self.gpio.set_servo_pulsewidth(self.servo_pin, Utils.translate(pulsewidth, 0, 5000, 500, 2450))
+        self.gpio.set_servo_pulsewidth(self.servo_pin, Utils.translate(pulsewidth, 0, 5000, 500, 2350))
